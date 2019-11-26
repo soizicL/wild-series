ef62c394656c3f4dfb61ca07d74ed639d90e2dca
soizic Lambert <soizic.lee@gmail.com>
2019-11-26 12:29:31 +0100

    add router quest symfony 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -19,4 +19,22 @@ Class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+
+    /**
+     * @Route("/wild/show/{slug}", name="wild_show", defaults={"slug" = ""},
+     * requirements={"slug" = "[a-z0-9\-]+"})
+     */
+    public function show(string $slug) : Response
+    {
+        $noSlug = "Aucune série séléctionnée, veuillez choisir une serie";
+        if($slug=== '') {
+             $slug=$noSlug;
+        } else {
+            $slug=ucwords(str_replace('-', ' ',$slug));
+        }
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
 }
